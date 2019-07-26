93d0086842afa1f36bcf3598c545cd2da0ded609
Ridikon <ridikon666@gmail.com>
2019-07-26 16:58:24 +0300

    implemented slides counter

--- js/main.js
+++ js/main.js
@@ -1,22 +1,46 @@
-let slides = null;
+let slides = null,
+    perPage = 10,
+    sliderInterval = 1,
+    counter = 0;
+
+const clientId = '99ab3726f51790918578fcbab308f1191c07f8e4ae37a7bd71b31e28276f8313';
+const url = `https://api.unsplash.com/photos/?per_page=${perPage}&client_id=${clientId}`;
+
+const slider = document.querySelector('#carousel');
 const prev = document.querySelector('#prev');
 const next = document.querySelector('#next');
+const selector = document.querySelector('#selector');
+const interval = document.querySelector('#interval');
+const menu = document.querySelector('#menu');
 
-const perPage = 100;
-const url = `https://api.unsplash.com/photos/?per_page=${perPage}&client_id=99ab3726f51790918578fcbab308f1191c07f8e4ae37a7bd71b31e28276f8313`;
-let counter = 0;
+function getPerPage() {
+    if (slides) {
+        slides.forEach(item => item.remove())
+    }
+    perPage = selector.value;
+    let newUrl = `https://api.unsplash.com/photos/?per_page=${perPage}&client_id=${clientId}`;
+    getImages(newUrl);
+}
+
+function getInterval() {
+    sliderInterval = interval.value;
+}
+
+function setMenuActive() {
+    menu.classList.toggle('menu-active');
+}
 
 function httpGet(url) {
     document.body.style.opacity = '0';
     document.body.style.display = 'none';
     document.body.style.transition = 'all .5s';
 
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
 
         let xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
 
-        xhr.onload = function() {
+        xhr.onload = function () {
             if (this.status === 200) {
                 resolve(this.response);
             } else {
@@ -26,7 +50,7 @@ function httpGet(url) {
             }
         };
 
-        xhr.onerror = function() {
+        xhr.onerror = function () {
             reject(new Error("Network Error"));
         };
 
@@ -34,26 +58,35 @@ function httpGet(url) {
     });
 }
 
-httpGet(url).then(response => {
-    document.body.style.display = 'block';
-    document.body.style.opacity = '1';
+function getImages(url) {
+    httpGet(url).then(response => {
+        document.body.style.display = 'block';
+        document.body.style.opacity = '1';
+        counter = 0;
 
-    const slidesData = JSON.parse(response);
-    const slider = document.querySelector('#carousel');
+        const slidesData = JSON.parse(response);
 
-    slidesData.forEach((item, i) => {
-        let elemImg = document.createElement('img');
-        elemImg.classList.add('carousel-item');
-        elemImg.setAttribute('src', item.urls.regular);
+        slidesData.forEach(item => {
+            let elemImg = document.createElement('img');
+            elemImg.classList.add('carousel-item');
+            elemImg.setAttribute('src', item.urls.regular);
 
-        if (i === 0) {
-            elemImg.classList.add('item-active')
-        }
+            slider.appendChild(elemImg);
+            slides = document.querySelectorAll('.carousel-item');
 
-        slider.appendChild(elemImg);
-        slides = document.querySelectorAll('.carousel-item');
+            slides.forEach((item, i) => {
+                item.classList.remove('item-active')
+                item.onclick = setMenuActive;
+
+                if (i === 0) {
+                    item.classList.add('item-active')
+                }
+            })
+        })
     })
-});
+}
+
+getImages(url);
 
 prev.onclick = () => {
     slides[counter].classList.remove('item-active');
