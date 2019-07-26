6b21489b40ea2d8390dc1bc8da3034232b24a49f
Vitalii <ridikon666@gmail.com>
2019-07-26 22:06:54 +0300

    implements play/pause slideshow

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 let slides = null,
-    perPage = 10,
-    sliderInterval = 1,
-    counter = 0;
+  perPage = 10,
+  sliderInterval = 1,
+  counter = 0,
+  playInterval = null;
 
 const clientId = '99ab3726f51790918578fcbab308f1191c07f8e4ae37a7bd71b31e28276f8313';
 const url = `https://api.unsplash.com/photos/?per_page=${perPage}&client_id=${clientId}`;
@@ -9,21 +10,43 @@ const url = `https://api.unsplash.com/photos/?per_page=${perPage}&client_id=${cl
 const slider = document.querySelector('#carousel');
 const prev = document.querySelector('#prev');
 const next = document.querySelector('#next');
-const selector = document.querySelector('#selector');
-const interval = document.querySelector('#interval');
+const pageSelector = document.querySelector('#pageSelector');
+const intervalSelector = document.querySelector('#intervalSelector');
 const menu = document.querySelector('#menu');
+const play = document.querySelector('#play');
+const pause = document.querySelector('#pause');
+
+play.onclick = playSlider;
+
+pause.onclick = stopSlider;
+
+function playSlider() {
+    pause.classList.remove('active');
+    play.classList.add('active');
+    playInterval = setInterval(() => nextSlide(), sliderInterval * 1000);
+}
+
+function stopSlider() {
+    play.classList.remove('active');
+    pause.classList.add('active');
+    if (playInterval) {
+        clearInterval(playInterval);
+    }
+}
+
+function changeInterval() {
+    stopSlider();
+    sliderInterval = intervalSelector.value;
+    playSlider();
+}
 
 function getPerPage() {
     if (slides) {
         slides.forEach(item => item.remove())
     }
-    perPage = selector.value;
+    perPage = pageSelector.value;
     let newUrl = `https://api.unsplash.com/photos/?per_page=${perPage}&client_id=${clientId}`;
     getImages(newUrl);
-}
-
-function getInterval() {
-    sliderInterval = interval.value;
 }
 
 function setMenuActive() {
@@ -98,7 +121,9 @@ prev.onclick = () => {
     slides[counter].classList.add('item-active');
 };
 
-next.onclick = () => {
+next.onclick = nextSlide;
+
+function nextSlide() {
     slides[counter].classList.remove('item-active');
     counter++;
 
@@ -107,4 +132,4 @@ next.onclick = () => {
     }
 
     slides[counter].classList.add('item-active');
-};
+}
